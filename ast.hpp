5dc8ef8982ed2d778e2c5f51804b2d9ecc25b71f
harsath <harsath@mm.st>
2022-10-11 02:32:16 -0400

    add str for tracing AST

--- ast.hpp
+++ ast.hpp
@@ -1,14 +1,15 @@
 #pragma once
 #include "lexer.hpp"
 #include "token.hpp"
+#include <memory>
 #include <string>
 #include <vector>
-#include <memory>
 
 namespace Ast {
 class Node {
       public:
 	virtual std::string tokenLiteral() = 0;
+	virtual std::string string() = 0;
 };
 
 class Statement : public Node {
@@ -18,8 +19,7 @@ class Statement : public Node {
 
 class Expression : public Node {
       public:
-	virtual void expressionNode(){}
-	virtual std::string tokenLiteral() override { return ""; }
+	virtual void expressionNode() = 0;
 };
 
 // This node is going to be root node of every AST our parser produces
@@ -33,6 +33,13 @@ class Program : public Node {
 			return "";
 		}
 	}
+	std::string string() override {
+		std::string returner;
+		for (std::shared_ptr<Statement> statement : this->statements) {
+			returner += statement->string();
+		}
+		return returner;
+	}
 };
 
 class Identifier : public Expression {
@@ -40,28 +47,56 @@ class Identifier : public Expression {
 	Lex::Token token;
 	void expressionNode() override {}
 	std::string tokenLiteral() override { return this->token.literal; }
+	std::string string() override { return this->token.literal; }
 };
 
 class LetStatement : public Statement {
-      // let IDENTIFIER = EXPRESSION;
+	// let IDENTIFIER = EXPRESSION;
       public:
 	Lex::Token token; // LET token
 	std::shared_ptr<Identifier> name;
 	std::shared_ptr<Expression> value;
 	void statementNode() override {}
-	std::string tokenLiteral() override {
-		return this->token.literal; 
+	std::string tokenLiteral() override { return this->token.literal; }
+	std::string string() override {
+		std::string returner;
+		returner += this->token.literal + " ";
+		returner += this->name->string() + " = ";
+		if (this->value != nullptr) { this->value->string(); }
+		returner += ";";
+		return returner;
 	}
 };
 
 class ReturnStatement : public Statement {
 	// return EXPRESSION;
-	public:
-		Lex::Token token; // RETURN token
-		std::shared_ptr<Expression> return_value;
-		void statementNode() override {}
-		std::string tokenLiteral() override {
-			return this->token.literal;
+      public:
+	Lex::Token token; // RETURN token
+	std::shared_ptr<Expression> return_value;
+	void statementNode() override {}
+	std::string tokenLiteral() override { return this->token.literal; }
+	std::string string() override {
+		std::string returner;
+		returner += this->token.literal + " ";
+		if (this->return_value != nullptr) {
+			returner += return_value->string();
 		}
+		return returner;
+	}
+};
+
+class ExpressionStatement : public Statement {
+	// EXPRESSION
+      public:
+	Lex::Token token;
+	std::shared_ptr<Expression> expression;
+	void statementNode() override {}
+	std::string tokenLiteral() override { return this->token.literal; }
+	std::string string() override {
+		std::string returner;
+		if (this->expression != nullptr) {
+			returner += this->expression->string();
+		}
+	}
 };
 } // namespace Ast
