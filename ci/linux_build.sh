6929f01a7d8cd89a8c432b29ea84231e8a6c3ccf
harsath <harsath@mm.st>
2022-10-30 21:05:46 -0400

    ci

--- ci/linux_build.sh
+++ ci/linux_build.sh
@@ -4,5 +4,6 @@ sudo apt update -y
 sudo apt install gcc-9 g++-9 ninja-build cmake -y
 mkdir build && cd build
 cmake .. && make
-./lexer_test
-./parser_test
+./tests/lexer_test
+./tests/parser_test
+./tests/ast_test
